db168da79a7a3e01e816b415c7776923fed76235
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-11-16 00:17:11 +0500

    feat: add category ui components

---
+++ bot/ui/category/list.py
@@ -0,0 +1,37 @@
+import enum
+from abc import ABC
+
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import InlineKeyboardMarkup
+
+from bot.ui import common
+from bot.ui.telegram_component import TelegramMarkup
+from core.domain.category import Category
+
+
+class CategoryViewMarkup(TelegramMarkup, ABC):
+    def __init__(self, category: Category):
+        self.category = category
+
+    def build(self) -> InlineKeyboardMarkup:
+        return common.create_markup(
+            (
+                ("Изменить имя", CategoryViewAction.edit_name),
+                ("Изменить описание", CategoryViewAction.edit_desc),
+                ("Файлы", CategoryViewAction.files_list)
+            ),
+            CategoryViewData,
+            "action",
+            category_id=self.category.id
+        )
+
+
+class CategoryViewAction(enum.StrEnum):
+    edit_name = enum.auto()
+    edit_desc = enum.auto()
+    files_list = enum.auto
+
+
+class CategoryViewData(CallbackData, prefix="category_view"):
+    action: CategoryViewAction
+    category_id: int
---
+++ bot/ui/category/set_category.py
@@ -0,0 +1,8 @@
+from abc import ABC
+
+from aiogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup
+
+from bot.ui import common
+from bot.ui.telegram_component import TelegramMarkup
+
+

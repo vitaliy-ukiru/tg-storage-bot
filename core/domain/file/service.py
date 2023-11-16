81069bbbc9b646e56d4d2022edee0851ddceeb56
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-11-16 19:20:38 +0500

    feat(service): add get file method

--- core/domain/file/service.py
+++ core/domain/file/service.py
@@ -1,7 +1,7 @@
 from asyncpg import UniqueViolationError
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import async_sessionmaker
-from sqlalchemy.orm import noload
+from sqlalchemy.orm import noload, lazyload, joinedload
 
 from core.domain.file import File, NamedCategory
 from core import database as db
@@ -36,7 +36,6 @@ class FileService:
                 user_id=db_file.user_id,
                 created_at=db_file.created_at,
                 title=db_file.title,
-
             )
 
             # TODO: improve this code
@@ -44,6 +43,31 @@ class FileService:
                 sql = select(db.Category).where(db.Category.id == db_file.category_id).options(noload())
                 res = await session.execute(sql)
                 category = res.scalar()
+                file.category = NamedCategory(category.id, category.title)
+
+            return file
+
+    async def get_file_by_id(self, file_id: int) -> File:
+        async with self._pool() as session:
+            sql = select(db.File).where(db.File.id == file_id).options(
+                joinedload(db.File.category),
+                joinedload(db.File.user)
+            )
+            res = await session.execute(sql)
+            db_file = res.scalar()
+
+            file = File(
+                file_id=db_file.id,
+                file_type=db_file.type_id,
+                remote_file_id=db_file.remote_id,
+                user_id=db_file.user_id,
+                created_at=db_file.created_at,
+                title=db_file.title,
+            )
+
+            # TODO: improve this code
+            if db_file.category_id is not None:
+                category = db_file.category
                 file.category = NamedCategory(category.id, category.title)
 
             return file

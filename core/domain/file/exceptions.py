35b15c14d7bdd755635e5080ae86cd25303749a0
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-11-22 00:58:29 +0500

    refactor: rename from remote_file_id to file_id
    That field now accepts and internal file_id

--- core/domain/file/exceptions.py
+++ core/domain/file/exceptions.py
@@ -1,19 +1,35 @@
+from core.domain.category import Category
 from core.domain.exceptions import DomainException
 
 
 class FileException(DomainException):
     domain_name = "file"
+    file_id: int | str
 
-    def __init__(self, remote_file_id: str, message: str) -> None:
-        self.remote_file_id = remote_file_id
+    def __init__(self, remote_file_id: int | str, message: str) -> None:
+        self.file_id = remote_file_id
         super().__init__(message)
 
     def __str__(self):
         message = super(Exception, self).__str__()
-        return f'domain.{self.domain_name}: file_id={self.remote_file_id} {message}'
+        return f'domain.{self.domain_name}: file_id={self.file_id} {message}'
+
+
+class FileNotFound(FileException):
+
+    def __init__(self, file_id: int | str) -> None:
+        super().__init__(file_id, "file not found")
 
 
 class FileAlreadyExists(FileException):
 
-    def __init__(self, remote_file_id: str) -> None:
-        super().__init__(remote_file_id, "file with same remote id already exists")
+    def __init__(self, file_id: int | str) -> None:
+        super().__init__(file_id, "file with same remote id already exists")
+
+
+class FileCategoryViolation(FileException):
+    category: str | int | Category
+
+    def __init__(self, file_id: int | str, category: str | int | Category) -> None:
+        self.category = category
+        super().__init__(file_id, f"user don't own category {category!r}")

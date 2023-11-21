c85e85db3ab43351aeb9e91b9dec7f69e4b2c760
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-11-22 00:57:32 +0500

    feat: implement file service

--- core/domain/file/service.py
+++ core/domain/file/service.py
@@ -1,16 +1,49 @@
+import abc
+from typing import cast
+
 from asyncpg import UniqueViolationError
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import async_sessionmaker
-from sqlalchemy.orm import noload, lazyload, joinedload
+from sqlalchemy.orm import noload, joinedload
 
-from core.domain.file import File, NamedCategory
 from core import database as db
-from core.domain.file.dto import CreateFileDTO
-from core.domain.file.exceptions import FileAlreadyExists
+from core.domain.category import Category
+from core.domain.file import File
+from core.domain.file.dto import CreateFileDTO, ReloadFileDTO
+from core.domain.file.exceptions import FileAlreadyExists, FileCategoryViolation, FileNotFound
 
 
-class FileService:
+class FileUseCase(abc.ABC):
+    @abc.abstractmethod
+    async def create_file(self, file_dto: CreateFileDTO) -> File:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    async def get_file_by_id(self, file_id: int) -> File:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    async def update_title(self, file_id: int, title: str):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    async def reload_file(self, file_id: int, reload_file: ReloadFileDTO):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    async def delete_file(self, file_id: int):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    async def set_category(self, file_id: int, category: Category) -> File:
+        raise NotImplementedError
+
+
+class FileService(FileUseCase):
     _pool: async_sessionmaker
+
+    def __init__(self, session_maker: async_sessionmaker):
+        self._pool = session_maker
 
     async def create_file(self, file_dto: CreateFileDTO) -> File:
         async with self._pool() as session:
@@ -43,7 +76,7 @@ class FileService:
                 sql = select(db.Category).where(db.Category.id == db_file.category_id).options(noload())
                 res = await session.execute(sql)
                 category = res.scalar()
-                file.category = NamedCategory(category.id, category.title)
+                file.category = category.to_domain()
 
             return file
 
@@ -55,19 +88,41 @@ class FileService:
             )
             res = await session.execute(sql)
             db_file = res.scalar()
+            return db_file.to_domain()
 
-            file = File(
-                file_id=db_file.id,
-                file_type=db_file.type_id,
-                remote_file_id=db_file.remote_id,
-                user_id=db_file.user_id,
-                created_at=db_file.created_at,
-                title=db_file.title,
-            )
+    async def update_title(self, file_id: int, title: str):
+        async with self._pool() as session:
+            sql = update(db.File).where(id=file_id).values(title=title)
+            await session.execute(sql)
+            await session.commit()
 
-            # TODO: improve this code
-            if db_file.category_id is not None:
-                category = db_file.category
-                file.category = NamedCategory(category.id, category.title)
+    async def reload_file(self, file_id: int, reload_file: ReloadFileDTO):
+        values = reload_file.__dict__.copy()
+        if reload_file.title is None:
+            values.pop("title")
 
-            return file
+        async with self._pool() as session:
+            sql = update(db.File).where(id=file_id).values(**values)
+            await session.execute(sql)
+            await session.commit()
+
+    async def delete_file(self, file_id: int):
+        async with self._pool() as session:
+            await session.delete(db.File(id=file_id))
+            await session.commit()
+
+    async def set_category(self, file_id: int, category: Category) -> File:
+        async with self._pool() as session:
+            db_file = await session.get(db.File, file_id)
+            if db_file is None:
+                raise FileNotFound(str(file_id))
+
+            db_file = cast(db.File, db_file)
+            if db_file.user_id != category.user_id:
+                raise FileCategoryViolation(db_file.remote_id, category)
+
+            db_file.category_id = category.id
+            await session.merge(db_file)
+            await session.commit()
+
+            return db_file.to_domain()

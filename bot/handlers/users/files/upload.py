3534e48b256dcde9ad02994e8326bcd9c2cdc6e0
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-11-22 02:58:51 +0500

    rework: uploader

--- bot/handlers/users/files/upload.py
+++ bot/handlers/users/files/upload.py
@@ -8,8 +8,21 @@ from bot.utils.uploader import FileUploader
 router = Router(name="base-upload-files")
 
 
-@router.message(MediaFilter())
-async def process_upload_file(msg: Message, uploader: FileUploader):
-    file = await uploader.upload_by_content_type(msg)
-    view = FileView(file)
-    await view.send(msg.bot, msg.chat.id)
+@router.message(MediaFilter(), StateFilter(None),)
+async def process_upload_file(msg: Message, uploader: FileUploader, dialog_manager: DialogManager):
+    if dialog_manager.current_context():
+        return
+
+    file = await uploader.upload(msg)
+    await dialog_manager.start(ViewSG.main, dict(file_id=file.id))
+
+@router.message(Command("file"))
+async def file_cmd(msg: Message, command: CommandObject, dialog_manager: DialogManager):
+    args = command.args
+    try:
+        file_id = int(args)
+    except:
+        await msg.answer("invalid file_id")
+        return
+    await dialog_manager.start(ViewSG.main, dict(file_id=file_id), mode=StartMode.RESET_STACK)
+

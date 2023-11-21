c4bf75217e26e60f5ecafd17c3893e12303afffc
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-11-22 00:59:23 +0500

    change order of fields, add dataclass decorator

--- core/domain/category/models.py
+++ core/domain/category/models.py
@@ -1,10 +1,12 @@
+from dataclasses import dataclass
 from datetime import datetime
+from typing import Optional
 
 
+@dataclass
 class Category:
     id: int
     user_id: int
-
     title: str
-    description: str | None
     created_at: datetime
+    description: Optional[str] = None

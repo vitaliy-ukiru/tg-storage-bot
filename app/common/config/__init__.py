65ae73a4768be9a161a6041b4812d4a53ace3913
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-12-16 22:55:36 +0500

    feat: add configration system
    Main entity is Loader. You can pass direct config to constructor or call "Loader.read" for read yaml config. Also, you can pass path in env var CONFIG_PATH

---
+++ app/common/config/__init__.py
@@ -0,0 +1,9 @@
+__all__ = (
+    'Loader',
+    'Config',
+    'DbConfig',
+    'TgBot',
+)
+
+from .config import Config, DbConfig, TgBot
+from .loader import Loader
---
+++ app/common/config/config.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from typing import Any, Optional, Callable
+
+from environs import Env
+
+
+@dataclass
+class DbConfig:
+    password: Optional[str]
+    username: str
+    database: str
+    host: Optional[str]
+    port: Optional[int]
+@dataclass
+class TgBot:
+    token: str
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    db: DbConfig
+
+
+
+
+
+
+

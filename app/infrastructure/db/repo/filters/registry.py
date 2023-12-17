1ef21c805385a6402751108737ed1c80a6be7c2c
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-12-17 14:07:52 +0500

    refactor: create registry for all types filters

---
+++ app/infrastructure/db/repo/filters/registry.py
@@ -0,0 +1,44 @@
+from typing import TypeVar
+
+from sqlalchemy import ColumnExpressionArgument
+
+from app.core.domain.exceptions.base import InvalidFilterError
+from app.core.interfaces.repository.file import FilterField
+from .base import Filter
+
+T = TypeVar('T')
+
+
+class Container:
+    _reg: dict[str, Filter]
+
+    def __init__(self):
+        self._reg = {}
+
+    def __call__(self, name: str):
+        return self.register(name)
+
+    def get(self, name: str) -> Filter | None:
+        return self._reg.get(name)
+
+    def names(self) -> tuple[str]:
+        return tuple(self._reg.keys())
+
+    def register(self, name: str):
+        def wrapper(func: Filter):
+            self._reg[name] = func
+            return func
+
+        return wrapper
+
+    def convert(self, f: FilterField[T]) -> ColumnExpressionArgument[bool]:
+        filter_func = self.get(f.name)
+        if filter_func is None:
+            raise InvalidFilterError(f.name)
+
+        return filter_func(f.value)
+
+
+class Registry:
+    files = Container()
+    categories = Container()

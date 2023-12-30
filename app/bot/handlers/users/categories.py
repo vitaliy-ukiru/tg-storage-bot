2db0f35ab70c815d38ccbc21300af3f13bcdbf2a
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-12-30 22:12:53 +0500

    feat: add command to create category

--- app/bot/handlers/users/categories.py
+++ app/bot/handlers/users/categories.py
@@ -5,11 +5,12 @@ from aiogram_dialog import DialogManager, StartMode
 
 from app.bot.filters.media import MediaFilter
 from app.bot.handlers.dialogs import execute
+from app.bot.states.dialogs import CategoryCreateSG
 from app.bot.utils.uploader import FileUploader
 
 router = Router(name="categories")
 @router.message(Command("cat"))
-async def file_cmd(msg: Message, command: CommandObject, dialog_manager: DialogManager):
+async def category_cmd(msg: Message, command: CommandObject, dialog_manager: DialogManager):
     args = command.args
     try:
         category_id = int(args)
@@ -17,3 +18,7 @@ async def file_cmd(msg: Message, command: CommandObject, dialog_manager: DialogM
         await msg.answer("invalid category_id")
         return
     await execute.category_edit(dialog_manager, category_id, mode=StartMode.RESET_STACK)
+
+@router.message(Command("create_category"))
+async def create_category(msg: Message, dialog_manager: DialogManager):
+    await dialog_manager.start(CategoryCreateSG.input_title)

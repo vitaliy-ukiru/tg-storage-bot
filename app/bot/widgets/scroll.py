409d83f54b05c332fb92916ed414f98a986b499c
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-12-30 21:33:29 +0500

    feat: add shortcut for navigation bar

---
+++ app/bot/widgets/scroll.py
@@ -0,0 +1,23 @@
+from typing import Optional, Union
+
+from aiogram_dialog.widgets.common import WhenCondition, Scroll
+from aiogram_dialog.widgets.kbd import (
+    Row,
+    FirstPage,
+    CurrentPage,
+    NextPage,
+    PrevPage, LastPage)
+from aiogram_dialog.widgets.text import Format
+
+
+class Navigation(Row):
+    def __init__(self, scroll_id: Union[str, Scroll], id: Optional[str] = None, when: WhenCondition = None):
+        super().__init__(
+            FirstPage(scroll=scroll_id, text=Format("<< {target_page1}")),
+            PrevPage(scroll=scroll_id),
+            CurrentPage(scroll=scroll_id),
+            NextPage(scroll=scroll_id),
+            LastPage(scroll=scroll_id, text=Format("{target_page1} >>")),
+            id=id,
+            when=when
+        )

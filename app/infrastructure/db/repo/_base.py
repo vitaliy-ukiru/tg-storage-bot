2c59e11a6549b4a7fee16a4145f9ab6f03fe281a
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2023-12-30 21:29:37 +0500

    feat(base repo): add apply_filters

--- app/infrastructure/db/repo/_base.py
+++ app/infrastructure/db/repo/_base.py
@@ -1,9 +1,11 @@
-from typing import Optional
+from typing import Optional, Sequence
 
 from sqlalchemy import Select
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from app.core.domain.dto.common import Pagination
+from app.core.interfaces.repository.common import FilterField
+from app.infrastructure.db.repo.filters.container import Container
 
 
 class BaseRepository:
@@ -13,10 +15,15 @@ class BaseRepository:
         self._pool = session_maker
 
     @staticmethod
+    def apply_filters(stmt: Select, reg: Container, filters: Sequence[FilterField]) -> Select:
+        for f in filters:
+            stmt = stmt.where(reg.convert(f))
+        return stmt
+
+    @staticmethod
     def apply_pagination(stmt: Select, p: Optional[Pagination]) -> Select:
         if p is None:
             return stmt
-
         if p.limit is not None:
             stmt = stmt.limit(p.limit)
         if p.offset is not None:

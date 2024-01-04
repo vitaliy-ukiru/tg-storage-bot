c3244de4cce3f18fdda4f588e062ea30df72dab8
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-01-04 21:23:09 +0500

    feat: add ordering entities

--- app/core/domain/dto/common.py
+++ app/core/domain/dto/common.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from enum import StrEnum, unique
 from typing import Optional
 
 
@@ -6,3 +7,26 @@ from typing import Optional
 class Pagination:
     limit: Optional[int] = None
     offset: Optional[int] = None
+
+
+@unique
+class OrderFields(StrEnum):
+    category_id = "category.id"
+    category_user = "category.user"
+    category_title = "category.title"
+    category_description = "category.description"
+    category_is_favorite = "category.is_favorite"
+    category_created_at = "category.created_at"
+
+    file_id = "file.id"
+    file_title = "file.title"
+    file_type = "file.type"
+    file_user = "file.user"
+    file_category = "file.category"
+    file_created_at = "file.created_at"
+
+
+@dataclass
+class Ordering:
+    field: OrderFields
+    reverse: bool = False

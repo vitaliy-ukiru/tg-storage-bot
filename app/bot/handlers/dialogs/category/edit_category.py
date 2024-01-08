050dd96954fb7b339f572298949e113d9512cbe2
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-01-08 23:14:49 +0500

    feat: inject dao using to edit category dialog

--- app/bot/handlers/dialogs/category/edit_category.py
+++ app/bot/handlers/dialogs/category/edit_category.py
@@ -1,20 +1,50 @@
-from typing import Optional
+from functools import wraps
+from typing import Optional, Any, Callable, TypeAlias, Awaitable
 
-from aiogram.types import Message, CallbackQuery
-from aiogram_dialog import Dialog, Window, DialogManager
-from aiogram_dialog.widgets.input import MessageInput, TextInput, ManagedTextInput
+from aiogram.fsm.state import State
+from aiogram.types import CallbackQuery
+from aiogram_dialog import Dialog, Window, DialogManager, ChatEvent
+from aiogram_dialog.widgets.common import ManagedWidget
+from aiogram_dialog.widgets.input import TextInput
 from aiogram_dialog.widgets.kbd import Button, SwitchTo, Back, Group, Cancel, Row, Checkbox, \
     Column, ManagedCheckbox
 from aiogram_dialog.widgets.text import Const, Format, Multi
 
 from app.bot.states.dialogs import CategoryEditSG
 from app.bot.widgets import BACK_TEXT, BackTo, CLOSE_TEXT
+from app.bot.widgets.dao import DialogDataProp, DialogDataRequiredProp
+from app.bot.widgets.dao.base_dao import BaseDAO
 from app.core.domain.dto.category import UpdateCategoryDTO
 from app.core.domain.models.category import CategoryId, Category
 from app.core.interfaces.usecase.category import CategoryUsecase
 
-_ON_START_SET_DATA = "__on_startup_setup"
+_ON_START_SET_DATA = "__on_startup_setup$$$$$"
 _FAVORITE_ID = "favorite"
+
+
+class UpdateDAO(BaseDAO):
+    category_id: CategoryId = DialogDataRequiredProp[CategoryId]("category_id")
+    __category_cache = DialogDataProp[Category | Any]("category")
+
+    async def get_category(self) -> Category:
+        category = self.__category_cache
+        if isinstance(category, Category):
+            return category
+
+        svc = self.category_service
+
+        category = await svc.get_category(self.category_id)
+        self.__category_cache = category
+        return category
+
+    @property
+    def category_service(self) -> CategoryUsecase:
+        return self.manager.middleware_data["category_service"]
+
+    def __category_setter(self, value: Category):
+        self.__category_cache = value
+
+    category = property(fset=__category_setter)
 
 
 async def _update_category(
@@ -24,8 +54,9 @@ async def _update_category(
     delete_desc: Optional[bool] = None,
     favorite: Optional[bool] = None,
 ) -> Category:
-    category_service: CategoryUsecase = manager.middleware_data["category_service"]
-    category_id: CategoryId = manager.dialog_data["category_id"]
+    data = UpdateDAO(manager)
+    category_service = data.category_service
+    category_id = data.category_id
     category = await category_service.update_category(UpdateCategoryDTO(
         category_id=category_id,
         title=title,
@@ -33,31 +64,37 @@ async def _update_category(
         delete_desc=delete_desc,
         favorite=favorite
     ))
-    manager.dialog_data["category"] = category
+    data.category = category
     return category
 
 
 async def _get_category(manager: DialogManager) -> Category:
-    category = manager.dialog_data.get("category")
-    if isinstance(category, Category):
-        return category
-
-    category_service: CategoryUsecase = manager.middleware_data["category_service"]
-    category_id: CategoryId = manager.dialog_data["category_id"]
-
-    category = await category_service.get_category(category_id)
-    manager.dialog_data["category"] = category
-    return category
+    dao = UpdateDAO(manager)
+    return await dao.get_category()
 
 
+CallbackType: TypeAlias = Callable[[ChatEvent, ManagedWidget, DialogManager, ...], Awaitable]
+
+
+def switcher(state: State):
+    def wrap_func(fn: CallbackType):
+        @wraps(fn)
+        async def wrapper(event, widget, manager: DialogManager, *args, **kwargs):
+            await fn(event, widget, manager, *args, **kwargs)
+            await manager.switch_to(state)
+
+        return wrapper
+
+    return wrap_func
+
+
+@switcher(CategoryEditSG.main)
 async def _input_title_handler(_, __, manager: DialogManager, text: str):
     await _update_category(manager, title=text)
-    await manager.switch_to(CategoryEditSG.main)
 
-
+@switcher(CategoryEditSG.main)
 async def _input_desc_handler(_, __, manager: DialogManager, text: str):
     await _update_category(manager, desc=text)
-    await manager.switch_to(CategoryEditSG.main)
 
 
 async def _process_click_favorite(event: CallbackQuery, m: ManagedCheckbox, manager: DialogManager):
@@ -68,9 +105,9 @@ async def _process_click_favorite(event: CallbackQuery, m: ManagedCheckbox, mana
     await manager.switch_to(CategoryEditSG.main)
 
 
+@switcher(CategoryEditSG.main)
 async def _process_delete_desc(_, __, manager: DialogManager):
     await _update_category(manager, delete_desc=True)
-    await manager.switch_to(CategoryEditSG.main)
 
 
 async def menu_getter(dialog_manager: DialogManager, **_):
@@ -80,12 +117,13 @@ async def menu_getter(dialog_manager: DialogManager, **_):
 
 async def _desc_window_getter(dialog_manager: DialogManager, **_):
     category = await _get_category(dialog_manager)
-    return dict(have_desc=category is not None)
+    return dict(have_desc=category.description is not None)
 
 
 async def _on_start(start_data: dict, manager: DialogManager):
-    manager.dialog_data["category_id"] = start_data["category_id"]
-    category = await _get_category(manager)
+    dao = UpdateDAO(manager)
+    dao.category_id = start_data["category_id"]
+    category = await dao.get_category()
 
     if category.is_favorite:
         m: ManagedCheckbox = manager.find(_FAVORITE_ID)
@@ -157,7 +195,3 @@ category_edit_dialog = Dialog(
     ),
     on_start=_on_start
 )
-
-
-class CategoryDescription:
-    description: Optional[str]

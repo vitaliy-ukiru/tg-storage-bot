93674fdba37de9b28b59d1561eb88ffdc7831aad
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-01-08 23:00:33 +0500

    refactor(dao/widgets): changed the base class to comply with the Liskov principle

--- app/bot/widgets/dao/widgets/base.py
+++ app/bot/widgets/dao/widgets/base.py
@@ -7,12 +7,11 @@ from abc import abstractmethod
 from typing import Generic, TypeVar
 
 from aiogram_dialog import DialogManager
-from aiogram_dialog.api.internal import Widget
 from aiogram_dialog.widgets.common import ManagedWidget
 
 from ..base import (ManagerGetterProvider, ManagerFieldProvider,
-                                      DialogManagerFieldProvider, ManagedDataObject, ManagerProviderVariant,
-                                      get_manager)
+                    DialogManagerFieldProvider, ManagerProviderVariant,
+                    get_manager, DataObjectABC)
 
 T = TypeVar('T')
 
@@ -24,7 +23,7 @@ ProviderT = TypeVar('ProviderT',
 ManagedWidgetT = TypeVar("ManagedWidgetT", bound=ManagedWidget)
 
 
-class WidgetDataObjectABC(Generic[T, ManagedWidgetT], ManagedDataObject[T]):
+class WidgetDataObjectABC(Generic[T, ManagedWidgetT], DataObjectABC[T]):
     def __init__(self, widget_id: str):
         self._widget_id = widget_id
 
@@ -40,17 +39,28 @@ class WidgetDataObjectABC(Generic[T, ManagedWidgetT], ManagedDataObject[T]):
     def process_del(self, widget: ManagedWidgetT):
         raise NotImplementedError
 
-    def prepare_args(self, obj: ManagerProviderVariant):
-        manager = get_manager(obj)
-        widget = self._get_widget(manager)
-        return (widget,)
-
     @property
     def widget_id(self) -> str:
         return self._widget_id
 
-    def _get_widget(self, m: DialogManager) -> ManagedWidget[T]:
+    def _find_widget(self, m: DialogManager) -> ManagedWidgetT:
         w = m.find(self._widget_id)
         if w is None:
             raise ValueError(f"Fail find widget {self._widget_id!r} in manager {m!r}")
         return w
+
+    def _get_widget(self, provider: ManagerProviderVariant) -> ManagedWidgetT:
+        manager = get_manager(provider)
+        return self._find_widget(manager)
+
+    def __get__(self, obj: ManagerProviderVariant, owner=None) -> T | None:
+        widget = self._get_widget(obj)
+        return self.process_get(widget)
+
+    def __set__(self, obj: ManagerProviderVariant, value: T):
+        widget = self._get_widget(obj)
+        self.process_set(widget, value)
+
+    def __delete__(self, obj: ManagerProviderVariant):
+        widget = self._get_widget(obj)
+        self.process_del(widget)

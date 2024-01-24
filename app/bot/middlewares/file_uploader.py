cb3fa01c8943536db5b64304bc7b7285d15b69de
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-01-25 02:38:41 +0500

    Revert "refactor: delete proxy middlewares"
    This reverts commit 122660ce772585d555e0af7f716eb770081feb82.

---
+++ app/bot/middlewares/file_uploader.py
@@ -0,0 +1,20 @@
+__all__ = (
+    'FileUploaderMiddleware',
+)
+from typing import Callable, Dict, Any, Awaitable
+
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+
+from app.bot.utils.uploader import FileUploader
+from app.core.interfaces.usecase.file import FileUsecase
+
+
+class FileUploaderMiddleware(BaseMiddleware):
+    def __init__(self, svc: FileUsecase):
+        self._uploader = FileUploader(svc)
+
+    async def __call__(self, handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+                       event: TelegramObject, data: Dict[str, Any]) -> Any:
+        data["uploader"] = self._uploader
+        return await handler(event, data)

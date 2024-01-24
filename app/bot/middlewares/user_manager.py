ba9f49f3d6c59349d00ff788e581d05e00e0601d
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-01-25 02:36:34 +0500

    refactor(bot/middleware): create user with lang code

--- app/bot/middlewares/user_manager.py
+++ app/bot/middlewares/user_manager.py
@@ -2,12 +2,16 @@ __all__ = (
     'UserMiddleware',
     'USER_KEY',
 )
+
 from typing import Callable, Dict, Any, Awaitable, cast
 
 from aiogram import BaseMiddleware
 from aiogram.dispatcher.middlewares.user_context import EVENT_FROM_USER_KEY
 from aiogram.types import TelegramObject, User as TgUser
 
+from app.bot.locales import ensure_locale
+from app.core.domain.dto.user import CreateUserDTO
+from app.core.domain.exceptions.user import UserNotFound
 from app.core.domain.models.user import UserId
 from app.core.interfaces.usecase import UserUsecase
 
@@ -18,14 +22,27 @@ class UserMiddleware(BaseMiddleware):
     def __init__(self, svc: UserUsecase):
         self.svc = svc
 
+    async def _get_user(self, tg_user: TgUser):
+        user_id = tg_user.id
+        try:
+            user = await self.svc.get_user(UserId(user_id), restore=True)
+        except UserNotFound:
+            user = await self.svc.create_user(
+                CreateUserDTO(
+                    user_id=user_id,
+                    locale=ensure_locale(tg_user.language_code),
+                )
+            )
+
+        return user
+
     async def __call__(self, handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
                        event: TelegramObject, data: Dict[str, Any]) -> Any:
         tg_user = cast(TgUser | None, data.get(EVENT_FROM_USER_KEY))
         if tg_user is None:
             return await handler(event, data)
 
-        user_id = tg_user.id
-        user = await self.svc.get_user(UserId(user_id), True)
+        user = await self._get_user(tg_user)
         data[USER_KEY] = user
         data["user_service"] = self.svc
         return await handler(event, data)

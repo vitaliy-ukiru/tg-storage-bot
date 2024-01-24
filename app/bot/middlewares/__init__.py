40244e30a2834027c5769cb25590a9850eee7112
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-01-25 03:11:18 +0500

    fix(middlewares): missing imports

--- app/bot/middlewares/__init__.py
+++ app/bot/middlewares/__init__.py
@@ -1,9 +1,5 @@
 __all__ = (
-    'CategoryProxyMiddleware',
-    'FileUploaderMiddleware',
-    'FileProxyMiddleware',
     'UserMiddleware'
 )
-from .file_uploader import FileUploaderMiddleware
-from .proxy_middlewares import CategoryProxyMiddleware, FileProxyMiddleware
+
 from .user_manager import UserMiddleware

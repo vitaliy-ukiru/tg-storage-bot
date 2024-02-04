3580536a1444d58209c502ed179986d07828b03a
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-02-04 12:42:07 +0500

    refactor: delete static start_data

--- app/bot/widgets/start_data.py
+++ app/bot/widgets/start_data.py
@@ -15,17 +15,15 @@ class StartWithData(Start):
                  text: Text,
                  id: str,
                  state: State,
-                 data: Data = None,
                  on_click: Optional[OnClick] = None,
                  getter: GetterVariant = None,
                  mode: StartMode = StartMode.NORMAL,
                  when: WhenCondition = None):
         self.getter = ensure_data_getter(getter)
-        super().__init__(text, id, state, data, on_click, mode, when)
+        super().__init__(text, id, state, None, on_click, mode, when)
 
     async def _on_click(self, callback: CallbackQuery, button: Button, manager: DialogManager):
         if self.user_on_click:
             await self.user_on_click(callback, self, manager)
         current_data = await self.getter(**manager.middleware_data)
-        start_data = self.start_data or {}
-        await manager.start(self.state, start_data | current_data, self.mode)
+        await manager.start(self.state, current_data, self.mode)

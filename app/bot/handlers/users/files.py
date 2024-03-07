2d6add7d387c2c9927b86d21077ed1f68cb04256
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-03-07 14:28:30 +0500

    feat(upload_file): auto add category when opened category_edit

--- app/bot/handlers/users/files.py
+++ app/bot/handlers/users/files.py
@@ -8,9 +8,10 @@ from aiogram_i18n import I18nContext
 from app.bot.filters.media import MediaFilter
 from app.bot.filters.via_self import ViaSelfRestrict
 from app.bot.handlers.dialogs import execute
-from app.bot.states.dialogs import ALLOWED_STATES
 from app.bot.services import FileUploader
+from app.bot.states.dialogs import ALLOWED_STATES, CategoryEditSG
 from app.core.domain.exceptions.file import FileAlreadyExists
+from app.core.domain.models.category import CategoryId
 from app.core.interfaces.usecase import FileUsecase
 
 router = Router()
@@ -29,6 +30,7 @@ async def process_upload_file(
     dialog_manager: DialogManager,
     i18n: I18nContext,
 ):
+    category_id: CategoryId | None = None
     # I don't have idea how get dialog_manager in filter
     # therefore will filter this and skip handler
     if dialog_manager.has_context():
@@ -36,11 +38,15 @@ async def process_upload_file(
         if state not in ALLOWED_STATES:
             raise SkipHandler
 
+        if state == CategoryEditSG.main:
+            category_id = dialog_manager.dialog_data["category_id"]
+
     uploader = FileUploader(file_service)
     try:
-        file = await uploader.upload(msg)
+
+        file = await uploader.upload(msg, category_id)
     except FileAlreadyExists:
-        await m.answer(i18n.get("file-already-exists"))
+        await msg.answer(i18n.get("file-already-exists"))
     else:
         await execute.file_view(dialog_manager, file.id)
 

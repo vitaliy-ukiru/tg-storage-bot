30b936792a35f7924192adb1ab079d0e4a69bcb8
Vitaliy Ukiru <33477790+vitaliy-ukiru@users.noreply.github.com>
2024-03-28 01:52:11 +0500

    feat: implement issuers

--- app/core/domain/models/auth.py
+++ app/core/domain/models/auth.py
@@ -37,3 +37,46 @@ class Issuer(ABC):
     @abstractmethod
     def issuer_type(self) -> IssuerType:
         return IssuerType.UNKNOWN
+
+class TelegramIssuer(Issuer):
+    def __init__(self, user_id: UserId):
+        self._user_id = user_id
+
+    @property
+    def user_id(self) -> UserId:
+        return self._user_id
+
+    def can(self, op: Operation) -> bool:
+        return True
+
+    @property
+    def issuer_type(self) -> IssuerType:
+        return IssuerType.TELEGRAM
+
+
+class ApiIssuer(Issuer):
+    def __init__(self, token: TokenInfo):
+        self._token = token
+
+    @property
+    def user_id(self) -> UserId:
+        return self._token.user_id
+
+    def can(self, op: Operation) -> bool:
+        _OPERATION_TO_SCOPE = {
+            Operation.category_create: Scope.category_create,
+            Operation.category_edit: Scope.category_edit,
+            Operation.file_create: Scope.file_create,
+            Operation.file_edit: Scope.file_edit,
+            Operation.file_delete: Scope.file_delete,
+        }
+
+        scope = _OPERATION_TO_SCOPE.get(op)
+        if not scope:
+            return False
+
+        return scope in self._token.scopes
+
+    @property
+    def issuer_type(self) -> IssuerType:
+        return IssuerType.TELEGRAM
